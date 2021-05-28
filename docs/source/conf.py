1611e86684747818daf3fb0c84b2218dffabaed9
Rishabh Shinde <62063908+RadoBoiii@users.noreply.github.com>
2021-05-28 13:26:37 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../FLASK/'))
 
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
