b51b0af0fcb0515f215229eb72bf11bd14875c34
Rishabh Shinde <62063908+RadoBoiii@users.noreply.github.com>
2021-05-28 14:12:33 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../FLASK/'))
 
-
+root_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = 'PDF-Extractor'
