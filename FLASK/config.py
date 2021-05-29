ca79e2c77668c342a646589b9f27d091008fda00
Rishabh Shinde <62063908+RadoBoiii@users.noreply.github.com>
2021-05-29 14:02:32 +0530

    Update config.py

--- FLASK/config.py
+++ FLASK/config.py
@@ -16,6 +16,6 @@ VALID_BATCH_SIZE = 16
 EPOCHS = 10
 
 # Add your paths here
-#META_MODEL_PATH = r"../FLASK/Resource/meta.bin"
-#MODEL_PATH = r"../FLASK/Resource/my_model.h5"
-#tokenizer = BertWordPieceTokenizer("https://github.com/RadoBoiii/PDF-Extractor-NER-BERT/blob/main/FLASK/bert-base-uncased-vocab.txt", lowercase=True)
+META_MODEL_PATH = r"Resource/meta.bin"
+MODEL_PATH = r"Resource/my_model.h5"
+tokenizer = BertWordPieceTokenizer("Resource/bert-base-uncased/bert-base-uncased-vocab.txt", lowercase=True)
