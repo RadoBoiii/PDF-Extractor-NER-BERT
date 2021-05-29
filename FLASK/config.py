14207479661f2a1da4eea0f173122de3ab6bbe72
Rishabh Shinde <62063908+RadoBoiii@users.noreply.github.com>
2021-05-29 13:35:08 +0530

    Update config.py

--- FLASK/config.py
+++ FLASK/config.py
@@ -18,4 +18,4 @@ EPOCHS = 10
 # Add your paths here
 META_MODEL_PATH = r"../FLASK/Resource/meta.bin"
 MODEL_PATH = r"../FLASK/Resource/my_model.h5"
-tokenizer = BertWordPieceTokenizer(r"../FLASK/Resource/bert_base_uncased/bert-base-uncased-vocab.txt", lowercase=True)
+tokenizer = BertWordPieceTokenizer(r"bert-base-uncased-vocab.txt", lowercase=True)
